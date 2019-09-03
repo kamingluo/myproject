64c65e447d8f546ca2657d200f94cd5cc7361c46
kaming <ljm@xlbzone.com>
2019-09-03 16:21:35 +0800

    ceshi

--- littlefun/php/application/miniapp/controller/game/Tenseconds.php
+++ littlefun/php/application/miniapp/controller/game/Tenseconds.php
@@ -37,6 +37,22 @@ class Tenseconds
     }
 
 
+
+    public function add(Request $request)
+    {
+    	  $wxcode =$request->param("code");
+		  $openid=openid($wxcode);
+    	  $redis = new Redis();  //实例化这个类
+          $gamenum=$redis->get($openid);
+          $newgame =$gamenum + 1 ;
+
+           $redis->set($openid,$newgame); //存入缓存，
+           return  $newgame;
+         
+        
+    }
+
+
    
 
  
