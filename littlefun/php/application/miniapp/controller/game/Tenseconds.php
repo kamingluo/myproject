25296a52570433f3347767bc2531663d6f49f093
kaming <ljm@xlbzone.com>
2019-09-03 15:11:54 +0800

    ceshi

--- littlefun/php/application/miniapp/controller/game/Tenseconds.php
+++ littlefun/php/application/miniapp/controller/game/Tenseconds.php
@@ -20,15 +20,18 @@ class Tenseconds
 		  $openid=openid($wxcode);
     	  $redis = new Redis();  //实例化这个类
           $gamenum=$redis->get($openid);
-          if($gamenum || $gamenum ==0 ){
-          	  //缓存存在
-          	  return 	$gamenum;
+          if(!$gamenum  ){
+
+          	 //缓存不存在
+          	$redis->set($openid, 0); //存入缓存，
+          	echo "----";
+          	$gamenum =0; //下发为0 
+          	return 	$gamenum;
+          	 
           	
           }else{
-          	  //缓存不存在
-          	$redis->set($openid, 0); //存入缓存，
-          	echo "1111";
-          	$gamenum =0; //下发为0 
+          	 //缓存存在
+          	  return 	$gamenum;
 
           }
         
