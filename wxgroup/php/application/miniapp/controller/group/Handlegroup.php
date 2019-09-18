46bc45fda4843ccf19c680e43776503744397ccf
kaming <ljm@xlbzone.com>
2019-09-18 10:38:22 +0800

    备注

--- wxgroup/php/application/miniapp/controller/group/Handlegroup.php
+++ wxgroup/php/application/miniapp/controller/group/Handlegroup.php
@@ -61,7 +61,7 @@ class Handlegroup
         $crowd_id=$request->param("crowd_id");//群id
         $user_id=$request->param("user_id");//用户id
 
-        $sql = "select  user.*,user_crowd.user_type,user_crowd.score,user_crowd.create_time as joincrowd_time from user,user_crowd where user.id=user_crowd.user_id and user_crowd.crowd_id =" .$crowd_id. " and user_crowd.user_id =" .$user_id. ";";
+        $sql = "select  user.*,user_crowd.user_type,user_crowd.score,user_crowd.remarks,user_crowd.create_time as joincrowd_time from user,user_crowd where user.id=user_crowd.user_id and user_crowd.crowd_id =" .$crowd_id. " and user_crowd.user_id =" .$user_id. ";";
         // return $sql;
         $querygroupuserdata = Db::query($sql); //拿到数据
         $state=['state'   => '200','message'  => "查询该群该用户的详细信息" ];
