fb964b6799d7b21c2460b849885fd67c8a46dc97
kaming <ljm@xlbzone.com>
2019-09-20 17:31:35 +0800

    兑换商品

--- wxgroup/wxminiapp/utils/share.js
+++ wxgroup/wxminiapp/utils/share.js
@@ -1,30 +1,15 @@
 var app = getApp();
 
-function share(type) {
-  let userdata = wx.getStorageSync('userdata')
+function share() {
+  //let userdata = wx.getStorageSync('userdata')
   // app.aldstat.sendEvent('分享好友', userdata);
-  let userid = wx.getStorageSync('userdata').id
-  let userchannel = wx.getStorageSync('userdata').channel
-  var shareconfig = wx.getStorageSync('shareconfig')
-  if (type == 1) { //button分享
-    return {
-      title: shareconfig.sharetitle,
-      desc: shareconfig.sharedesc,
-      imageUrl: shareconfig.shareimg,
-      path: '/pages/index/index?channel=' + userchannel + '&master_id=' + userid , // 路径，传递参数到指定页面。
+   return {
+      title: "推荐你一款好用群管理工具",
+      desc: "推荐你一款好用群管理工具",
+      imageUrl: 'http://material.gzywudao.top/tmp_2d810deba75dc33973ffd24232036e5e.png',
+      path: '/pages/index/index?channel=1002', // 路径，传递参数到指定页面。
     }
-    
-  } else {
-    return {
-      title: shareconfig.sharetitle,
-      desc: shareconfig.sharedesc,
-      imageUrl: shareconfig.shareimg,
-      path: '/pages/index/index?channel=' + userchannel + '&master_id=' + userid, // 路径，传递参数到指定页面。
-    }
-  }
 }
-
-
 
 module.exports = {
   share: share,
