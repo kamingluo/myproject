082866bb9a4b1e255c549a4a200f80e5f4e98731
kaming <ljm@xlbzone.com>
2019-09-24 18:16:53 +0800

    兑换商品

--- wxgroup/wxminiapp/pages/group/goods/exchange/exchange.js
+++ wxgroup/wxminiapp/pages/group/goods/exchange/exchange.js
@@ -11,22 +11,29 @@ Page({
    */
   data: {
     useraddress:null,
+    goods_id:null,
+    goodsdata:null
 
   },
 
   onLoad: function (options) {
     console.log(options.goods_id)
+    this.setData({
+      goods_id: options.goods_id,
+    })
 
   },
 
   onShow: function () {
     this.address()
+    this.goodsdata()
   },
 
   address:function(){
     var user_id = wx.getStorageSync('userdata').id
     request({
       service: 'user/useraddress',
+      method: 'GET',
       data: {
         user_id: user_id,
       },
@@ -43,6 +50,23 @@ Page({
   modifyaddress:function(){
     wx.navigateTo({
       url: '/pages/my/my_address/my_address'
+    })
+  },
+
+  goodsdata:function(){
+    let goods_id = this.data.goods_id
+    request({
+      service: 'group/groupgoods/goodsdetails',
+      method: 'GET',
+      data: {
+        goods_id: goods_id,
+      },
+      success: res => {
+        console.log(res)
+        this.setData({
+          goodsdata: res.data,
+        })
+      },
     })
   },
 
