6f57d3b73d57f36717c13226e7d43b40eae71e03
kaming <ljm@xlbzone.com>
2019-09-27 16:07:31 +0800

    清理过期的formid

--- wxgroup/php/application/miniapp/controller/temmsg/Exchange.php
+++ wxgroup/php/application/miniapp/controller/temmsg/Exchange.php
@@ -7,10 +7,21 @@ use think\Config;
 class Exchange
 {
 
+
+
+   public function clearformid()
+    {
+
+       $day=date("Y-m-d H:i:s", strtotime('-6 days'));//获取6天时间
+       $clearformid=db('formid')-> where('create_time','< time', $day)->delete();
+       return "共清理过期formid数量-->" .$clearformid ;
+
+    }
+
+
 	 //用户兑换给群主通知
     public function exchangesuccess(Request $request)
     {
-
     	//找出该群该群主最远的推送id;
     	//根据群id找到群主的openid
     	// $crowd_id=2;
@@ -18,19 +29,12 @@ class Exchange
     	// // return $crowd_owner_id;
     	// $temmsg_formid=db('formid')->where('openid',$crowd_owner_id)->value('formid');//拿到推送id
     	// return $temmsg_formid;
-
         $nickName="兑换昵称";
         $goodsname="商品名称";
         $price=100;
         $crowd_id=2;
-
-
-
-    	 $temmsg=userexchange($nickName,$goodsname,$price,$crowd_id);
- 
-    	
-    	return   $temmsg;
-
+    	  $temmsg=userexchange($nickName,$goodsname,$price,$crowd_id);
+    	  return   $temmsg;
      
     }
 
@@ -39,11 +43,8 @@ class Exchange
     public function exchangetest(Request $request)
     {
 
-    	
-    	
-    	$exchange_id=$request->param("exchange_id");//兑换记录id
-        $expressnumber=$request->param("expressnumber");//快递单号
-
+     $exchange_id=$request->param("exchange_id");//兑换记录id
+      $expressnumber=$request->param("expressnumber");//快递单号
        //  $exchangedata=db('exchange_record')->where('id',$exchange_id)->find(); //根据id找到兑换详情
        //  $temmsg_openid=$exchangedata["openid"];
        //  $temmsg_formid=db('formid')->where('openid',$temmsg_openid)->value('formid');//拿到推送id
@@ -54,19 +55,10 @@ class Exchange
        //      $expressnumber="无快递单号";
        //  }
        // $time =date('Y-m-d H:i:s',time());//获取当前时间
-
-    	
- 
     	$temdata=delivergoods($exchange_id,$expressnumber);
     	return   $temdata;
-
-     
     }
 
-   
-    
-
-    
 
 }
 
