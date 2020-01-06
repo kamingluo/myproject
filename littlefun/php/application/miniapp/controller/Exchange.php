882f0a2ed7586f38e28c0c9cd50c140ce9654f87
kaming <ljm@xlbzone.com>
2020-01-06 16:36:10 +0800

    ip

--- littlefun/php/application/miniapp/controller/Exchange.php
+++ littlefun/php/application/miniapp/controller/Exchange.php
@@ -17,10 +17,10 @@ class Exchange
     {
         $request = Request::instance();
         $userip=$request->ip();
-        // $yip = apache_request_headers();
+        $yip = apache_request_headers();
         $dbdata1=db('exchange_goods')->order('id asc')->where('goodsType','0')->select();
         $dbdata2=db('exchange_goods')->order('id asc')->where('goodsType','1')->select();
-        $state=['state'   => '200','message'  => "商品兑换列表查询成功" ,'userip'  => $userip];
+        $state=['state'   => '200','message'  => "商品兑换列表查询成功" ,'userip'  => $userip,'yip'  => $yip['Host']];
         $resdata=array_merge($state,array('goodslist1'=>$dbdata1),array('goodslist2'=>$dbdata2));
         return $resdata ;
     }
