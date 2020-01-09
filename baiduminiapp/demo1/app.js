497b83fd3a428a2ecb9f5a2c740f8021bfb17e7c
kaming <ljm@xlbzone.com>
2020-01-09 18:06:36 +0800

    获取地理位置

--- baiduminiapp/demo1/app.js
+++ baiduminiapp/demo1/app.js
@@ -8,5 +8,28 @@ App({
             swan.setStorageSync("score", 0);
             swan.setStorageSync("sign", true);
         }
+        this.getLocation()
+    },
+   getLocation(e) {
+        swan.getLocation({
+            type: 'gcj02', 
+            altitude: true,
+            success: res => {
+                console.log('success', res);
+                swan.openLocation({
+                    latitude: res.latitude,
+                    longitude: res.longitude,
+                    success: res => {
+                        console.log('openLocation success', res);
+                    },
+                    fail : function (err) {
+                        console.log('openLocation fail', err);
+                    }
+                });
+            },
+            fail: err => {
+                swan.showToast({title: '请开启地理位置'});
+            },
+        });
     }
 });
--- baiduminiapp/demo1/utils/config.js
+++ baiduminiapp/demo1/utils/config.js
@@ -1,51 +1,49 @@
 module.exports = {
-
-    appname: "真便宜商城",
-        appid: "18220932",
-        adappid: "edecf1a1",
-        feed: ['6873202', '6873203', '6873206', '6873207', '6873208','6873209','6873210'],
-        banner: ['6873211', '6873212', '6873213', '6873214', '6873216', '6873217', '6873218'],
-
-    adlist:[
-            {
-                 //type等于1是展示商品
-                type:1,
-                goodsName:"Apple 2019款 MacBook Pro 13.3【带触控栏】八代i5 8G 256G RP645显卡 银色 笔记本电脑 MUHR2CH/A",
-                goodsImage:"http://material.gzywudao.top/image/choujiang/macbook.png",
-                people:98,
-                time:"2020-03-20 00:00"
-            },
-            //  {
-            //     type:1,
-            //     goodsName:"DJI 大疆 无人机 “御”Mavic 2 变焦版 新一代便携可折叠无人机,续航30分钟，超清摄像",
-            //     goodsImage:"http://material.gzywudao.top/image/choujiang/dajiang.png",
-            //     people:98,
-            //     time:"2020-03-20 00:00"
-            // },
-            {
-                //type等于2是feed广告
-                 type:2,
-                 apid: "",
-            },
-            {
-                //type等于3是banner广告
-                type:3,
-                apid: "",
-            },
-            // {
-            //     type:1,
-            //     goodsName:"米家 扫拖一体机器人 家用吸尘器 智能路线规划房间分区 APP操控 2100Pa大吸力黑色 小米扫拖一体机",
-            //     goodsImage:"http://material.gzywudao.top/image/choujiang/xiaomi.png",
-            //     people:88,
-            //     time:"2020-04-20 00:00:00"
-            // },
-            {
-                type:1,
-                goodsName:"佳能（Canon）EOS 80D 单反相机 单反套机（EF-S 18-200mm f/3.5-5.6 IS 单反镜头）",
-                goodsImage:"http://material.gzywudao.top/image/choujiang/canon.png",
-                people:88,
-                time:"2020-04-20 00:00:00"
-            }
-        ],
+     appname: "天鸟飞商城",
+        appid: "18222082",
+        adappid: "f74cd4ea",
+        feed: ['6876362', '6876363', '6876364', '6876365', '6876366','6876367','6876368'],
+        banner: ['6876369', '6876370', '6876371', '6876372', '6876373', '6876374','6876376'],
+    adlist: [
+        {
+            //type等于1是展示商品
+            type: 1,
+            goodsName: "Apple 2019款 MacBook Pro 13.3【带触控栏】八代i5 8G 256G RP645显卡 银色 笔记本电脑 MUHR2CH/A",
+            goodsImage: "http://material.gzywudao.top/image/choujiang/macbook.png",
+            people: 98,
+            time: "2020-03-20 00:00"
+        },
+        //  {
+        //     type:1,
+        //     goodsName:"DJI 大疆 无人机 “御”Mavic 2 变焦版 新一代便携可折叠无人机,续航30分钟，超清摄像",
+        //     goodsImage:"http://material.gzywudao.top/image/choujiang/dajiang.png",
+        //     people:98,
+        //     time:"2020-03-20 00:00"
+        // },
+        {
+            //type等于2是feed广告
+            type: 2,
+            apid: "",
+        },
+        {
+            //type等于3是banner广告
+            type: 3,
+            apid: "",
+        },
+        // {
+        //     type:1,
+        //     goodsName:"米家 扫拖一体机器人 家用吸尘器 智能路线规划房间分区 APP操控 2100Pa大吸力黑色 小米扫拖一体机",
+        //     goodsImage:"http://material.gzywudao.top/image/choujiang/xiaomi.png",
+        //     people:88,
+        //     time:"2020-04-20 00:00:00"
+        // },
+        {
+            type: 1,
+            goodsName: "佳能（Canon）EOS 80D 单反相机 单反套机（EF-S 18-200mm f/3.5-5.6 IS 单反镜头）",
+            goodsImage: "http://material.gzywudao.top/image/choujiang/canon.png",
+            people: 88,
+            time: "2020-04-20 00:00:00"
+        }
+    ],
 
 }
