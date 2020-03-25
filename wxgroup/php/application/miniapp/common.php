618384449aae8c77f894569e4f87b8406949a960
kaming <ljm@xlbzone.com>
2020-03-25 10:50:31 +0800

    拿wxtoken

--- wxgroup/php/application/miniapp/common.php
+++ wxgroup/php/application/miniapp/common.php
@@ -66,23 +66,13 @@ function joingroup($crowd_id, $user_id, $user_openid){
 function userexchange($nickName,$goodsname,$price,$crowd_id){
     //根据群id找到群主的openid
       $crowd_owner_id=db('user_crowd')->where('crowd_id',$crowd_id)->where('user_type',1)->value('user_openid'); //拿到群主的openid
-
       $senopenid=$crowd_owner_id;
-      $data['appid']=Config('appid');
-      $data['secret']= Config('secret');
-      $data['grant_type']= 'client_credential';
-      $api = "https://api.weixin.qq.com/cgi-bin/token";//拿token接口
-      $str = http($api, $data,'GET');
-      $token = json_decode($str,true);
-      $access_token=$token['access_token'];//拿到token
-
+      $access_token=wxtoken();//拿到token
       $temid = 'zhWe1Om6o3IK-A7ruaJoGvrtshuD-H5Fg0UpMQrzseU';
       $page = 'pages/index/index';
       $url = 'https://api.weixin.qq.com/cgi-bin/message/subscribe/send?access_token='.$access_token;
-      Log::record("这里是开始啦");
       $explan="用户:".$nickName.",兑换商品";
       $time =date('Y-m-d H:i:s',time());//获取当前时间
-      
       $data = array(//这里一定要按照微信给的格式
         "touser"=>$senopenid,
         "template_id"=>$temid,
@@ -105,11 +95,9 @@ function userexchange($nickName,$goodsname,$price,$crowd_id){
          )
       );
     $res = postCurl($url,$data,'json');//将data数组转换为json数据
-    Log::record("这里是结束啦");
     if($res){
-        //推送完要把值删除
+        //推送完要把值删除,这是旧版需要的功能
          //$cleardata=db('formid')-> where('formid',$temmsg_formid)->delete();
-       Log::record($res);
        return "用户兑换成功推送成功";
        // echo json_encode(array('state'=>4,'msg'=>$res));
     }else{
@@ -135,24 +123,13 @@ function delivergoods($exchange_id,$expressnumber){
         if(!$expressnumber){
             $expressnumber="无快递单号";//先埋起来
         }
-   
       $senopenid=$temmsg_openid;
-      $data['appid']=Config('appid');
-      $data['secret']= Config('secret');
-      $data['grant_type']= 'client_credential';
-      $api = "https://api.weixin.qq.com/cgi-bin/token";//拿token接口
-      $str = http($api, $data,'GET');
-      $token = json_decode($str,true);
-      $access_token=$token['access_token'];//拿到token
-
+      $access_token=wxtoken();//拿到token
       $temid = 'SknRZZeUTqjuuOKPqxANRoZMl2jhUBJbwvd5P8JgjN8';
       $page = 'pages/index/index?exchangelist=true';
       $url = 'https://api.weixin.qq.com/cgi-bin/message/subscribe/send?access_token='.$access_token;
-      Log::record("这里是开始啦");
       $explan="群:".$crowd_name.";";
-      Log::record($explan);
       $time =date('Y-m-d H:i:s',time());//获取当前时间
-      
       $data = array(//这里一定要按照微信给的格式
         "touser"=>$senopenid,
         "template_id"=>$temid,
@@ -173,16 +150,35 @@ function delivergoods($exchange_id,$expressnumber){
         );
     $res = postCurl($url,$data,'json');//将data数组转换为json数据
     if($res){
-        //推送完要把值删除
-         //$cleardata=db('formid')-> where('formid',$temmsg_formid)->delete();
-         Log::record($res);
        return "群主发货推送成功";
-       // echo json_encode(array('state'=>4,'msg'=>$res));
     }else{
-         //$cleardata=db('formid')-> where('formid',$temmsg_formid)->delete();
         return "群主发货推送失败";
-        // echo json_encode(array('state'=>5,'msg'=>$res));
     }
 }
 
 
+
+//群主发货给用户通知，
+function wxtoken(){
+    $dbres =db('wxtoken')->where('id',1)->find();//查询用户有没有加入群
+    $token_time=$dbres["update_time"];
+    $time =date('Y-m-d H:i:s',time());//获取当前时间
+    $second=floor((strtotime($time)-strtotime($token_time)));//对比两个时间，拿到时间差
+    if($second > 3600){
+        //一小时更新一次,超过一小时再去调一次
+        $data['appid']=Config('appid');
+        $data['secret']= Config('secret');
+        $data['grant_type']= 'client_credential';
+        $api = "https://api.weixin.qq.com/cgi-bin/token";//拿token接口
+        $str = http($api, $data,'GET');
+        $token = json_decode($str,true);
+        $access_token=$token['access_token'];//拿到token
+        //更新一下数据库的access_token和时间
+        $updatedata= db('wxtoken')->where('id',1)->update(['update_time' => $time,'access_token' => $access_token]);
+    }
+    else{
+        $access_token=$dbres["access_token"];//直接拿到数据库存储的token
+    }
+    return $access_token;
+
+}
